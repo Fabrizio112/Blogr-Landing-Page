feb589f29914d4c3ba9def1671aaad0613935bbc
Fabrizio112 <fabri.avila3@gmail.com>
2023-05-02 19:27:40 -0300

    Agrega la funcion del menu desplegable al Nav

--- index.js
+++ index.js
@@ -12,3 +12,40 @@ $CRUZ.addEventListener("click", () => {
     let $menuDesplegable = document.querySelector("#nav-right");
     $menuDesplegable.style.display = "none";
 })
+
+const $PRODUCTS = document.querySelector("#products");
+let contadorProducts = 0;
+$PRODUCTS.addEventListener("click", () => {
+    let listaDesplegable = document.querySelector("#container-products");
+    if (contadorProducts === 1) {
+        listaDesplegable.style.display = "grid";
+        contadorProducts--;
+    } else {
+        listaDesplegable.style.display = "none";
+        contadorProducts++;
+    }
+})
+const $COMPANY = document.querySelector("#company");
+let contadorCompany = 0;
+$COMPANY.addEventListener("click", () => {
+    let listaDesplegable = document.querySelector("#container-company");
+    if (contadorCompany === 1) {
+        listaDesplegable.style.display = "grid";
+        contadorCompany--;
+    } else {
+        listaDesplegable.style.display = "none";
+        contadorCompany++;
+    }
+})
+const $CONNECT = document.querySelector("#connect");
+let contadorConnect = 0;
+$CONNECT.addEventListener("click", () => {
+    let listaDesplegable = document.querySelector("#container-connect");
+    if (contadorConnect === 1) {
+        listaDesplegable.style.display = "grid";
+        contadorConnect--;
+    } else {
+        listaDesplegable.style.display = "none";
+        contadorConnect++;
+    }
+})
